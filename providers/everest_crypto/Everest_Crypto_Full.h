ed0c79a0fef98cfc35ec0743245e04c1dbdb05ae
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2018-03-16 11:08:47 +0100

    Header file for Everest_Crypto provider

--- providers/everest_crypto/Everest_Crypto_Full.h
+++ providers/everest_crypto/Everest_Crypto_Full.h
@@ -22,18 +22,14 @@
  */
 
 #include "stdint.h"
-#include "Everest_Crypto.h"
-#include "Hacl_Curve25519.h"
-#include "Hacl_Chacha20.h"
-#include "Hacl_Poly1305_64.h"
+#ifndef __Everest_Crypto_H
+#define __Everest_Crypto_H
 
 //
 // Curve25519
 //
 
-void hacl_curve25519_crypto_scalarmult(uint8_t *mypublic, uint8_t *secret, uint8_t *basepoint) {
-  Hacl_Curve25519_crypto_scalarmult(mypublic, secret, basepoint);
-}
+void hacl_curve25519_crypto_scalarmult(uint8_t *mypublic, uint8_t *secret, uint8_t *basepoint);
 
 //
 // Chacha20
@@ -41,30 +37,18 @@ void hacl_curve25519_crypto_scalarmult(uint8_t *mypublic, uint8_t *secret, uint8
 
 typedef uint8_t* hacl_chacha20_state;
 
-void hacl_chacha20_setup(hacl_chacha20_state st, uint8_t* k, uint8_t* n, uint32_t c) {
-  Hacl_SecureAPI_Chacha20_setup(st, k, n, c);
-}
+void hacl_chacha20_setup(hacl_chacha20_state st, uint8_t* k, uint8_t* n, uint32_t c);
 
-void hacl_chacha20_stream(uint8_t* stream_block, hacl_chacha20_state st) {
-  Hacl_SecureAPI_stream(stream_block, st);
-}
+void hacl_chacha20_stream(uint8_t* stream_block, hacl_chacha20_state st);
 
-void hacl_chacha20_stream_finish(uint8_t* stream_block, uint32_t len, hacl_chacha20_state st) {
-  Hacl_SecureAPI_stream(stream_block, st);
-}
+void hacl_chacha20_stream_finish(uint8_t* stream_block, uint32_t len, hacl_chacha20_state st);
 
-void hacl_chacha20(uint8_t* output, uint8_t* plain, uint32_t len, uint8_t* key, uint8_t* nonce, uint32_t ctr) {
-  Hacl_Chacha20_chacha20(output, plain, len, key, nonce, ctr);
-}
+void hacl_chacha20(uint8_t* output, uint8_t* plain, uint32_t len, uint8_t* key, uint8_t* nonce, uint32_t ctr);
 
 //
 // Poly1305
 //
 
-void hacl_poly1305_64(uint8_t *output, uint8_t *input, uint64_t len, uint8_t *key)
-  Hacl_Poly1305_64_crypto_onetimeauth(output, input, len, key);
-)
+void hacl_poly1305_64(uint8_t *output, uint8_t *input, uint64_t len, uint8_t *key);
 
-void vale_poly1305_64(uint8_t *output, uint8_t *input, uint64_t len, uint8_t *key)
-  Hacl_Poly1305_64_crypto_onetimeauth(output, input, len, key);
-)
+void vale_poly1305_64(uint8_t *output, uint8_t *input, uint64_t len, uint8_t *key);
