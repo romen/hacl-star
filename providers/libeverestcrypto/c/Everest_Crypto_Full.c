252556de6e75f51d6e3066468985ed7c16631e67
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2018-03-19 08:36:38 +0100

    Prefixing for libeverestcryto symbols

--- providers/libeverestcrypto/c/Everest_Crypto_Full.c
+++ providers/libeverestcrypto/c/Everest_Crypto_Full.c
@@ -34,12 +34,26 @@
 // Curve25519
 //
 
-void crypto_hacl_curve25519_scalarmult(uint8_t *mypublic, uint8_t *secret, uint8_t *basepoint) {
+void everestcrypto_hacl_curve25519_scalarmult(uint8_t *mypublic, uint8_t *secret, uint8_t *basepoint) {
   Hacl_Curve25519_crypto_scalarmult(mypublic, secret, basepoint);
 }
 
-void crypto_openssl_curve25519_scalarmult(uint8_t *mypublic, uint8_t *secret, uint8_t *basepoint) {
+void everestcrypto_openssl_curve25519_scalarmult(uint8_t *mypublic, uint8_t *secret, uint8_t *basepoint) {
   X25519(mypublic, secret, basepoint);
+}
+
+//
+// AES
+//
+
+void everestcrypto_vale_aes_keyExpansion(uint8_t *k, uint8_t *w, uint8_t *sb)
+{
+  Vale_AES_keyExpansion(k, w, sb);
+}
+
+void everestcrypto_vale_aes_cipher(uint8_t *out, uint8_t *in, uint8_t *w, uint8_t *sb)
+{
+  Vale_AES_cipher(out, in, w, sb);
 }
 
 //
@@ -48,19 +62,19 @@ void crypto_openssl_curve25519_scalarmult(uint8_t *mypublic, uint8_t *secret, ui
 
 typedef uint8_t* hacl_chacha20_state;
 
-void crypto_hacl_chacha20_setup(hacl_chacha20_state st, uint8_t* k, uint8_t* n, uint32_t c) {
+void everestcrypto_hacl_chacha20_setup(everestcrypto_hacl_chacha20_state st, uint8_t* k, uint8_t* n, uint32_t c) {
   Hacl_SecureAPI_Chacha20_setup(st, k, n, c);
 }
 
-void crypto_hacl_chacha20_stream(uint8_t* stream_block, hacl_chacha20_state st) {
+void everestcrypto_hacl_chacha20_stream(uint8_t* stream_block, everestcrypto_hacl_chacha20_state st) {
   Hacl_SecureAPI_stream(stream_block, st);
 }
 
-void crypto_hacl_chacha20_stream_finish(uint8_t* stream_block, uint32_t len, hacl_chacha20_state st) {
+void everestcrypto_hacl_chacha20_stream_finish(uint8_t* stream_block, uint32_t len, everestcrypto_hacl_chacha20_state st) {
   Hacl_SecureAPI_stream(stream_block, st);
 }
 
-void crypto_hacl_chacha20(uint8_t* output, uint8_t* plain, uint32_t len, uint8_t* key, uint8_t* nonce, uint32_t ctr) {
+void everestcrypto_hacl_chacha20(uint8_t* output, uint8_t* plain, uint32_t len, uint8_t* key, uint8_t* nonce, uint32_t ctr) {
   Hacl_Chacha20_chacha20(output, plain, len, key, nonce, ctr);
 }
 
@@ -68,11 +82,11 @@ void crypto_hacl_chacha20(uint8_t* output, uint8_t* plain, uint32_t len, uint8_t
 // Poly1305
 //
 
-void crypto_hacl_poly1305_64(uint8_t *output, uint8_t *input, uint64_t len, uint8_t *key) {
+void everestcrypto_hacl_poly1305_64(uint8_t *output, uint8_t *input, uint64_t len, uint8_t *key) {
   Hacl_Poly1305_64_crypto_onetimeauth(output, input, len, key);
 }
 
-void crypto_vale_poly1305_64(uint8_t *output, uint8_t *input, uint64_t len, uint8_t *key) {
+void everestcrypto_vale_poly1305_64(uint8_t *output, uint8_t *input, uint64_t len, uint8_t *key) {
   Hacl_Poly1305_64_crypto_onetimeauth(output, input, len, key);
 }
 
@@ -80,33 +94,18 @@ void crypto_vale_poly1305_64(uint8_t *output, uint8_t *input, uint64_t len, uint
 // SHA2_256
 //
 
-void crypto_vale_sha2_256_init(uint32_t *state) {
+void everestcrypto_vale_sha2_256_init(uint32_t *state) {
   Vale_Hash_SHA2_256_init(state);
 }
 
-void crypto_vale_sha2_256_update(uint32_t *state, uint8_t *data) {
+void everestcrypto_vale_sha2_256_update(uint32_t *state, uint8_t *data) {
   Vale_Hash_SHA2_256_update(state, data);
 }
 
-void crypto_vale_sha2_256_update_last(uint32_t *state, uint8_t *data, uint32_t *len) {
+void everestcrypto_vale_sha2_256_update_last(uint32_t *state, uint8_t *data, uint32_t *len) {
   Vale_Hash_SHA2_256_update_last(state, data, len);
 }
 
-void crypto_vale_sha2_256_finish(uint32_t *state, uint8_t *dst) {
+void everestcrypto_vale_sha2_256_finish(uint32_t *state, uint8_t *dst) {
   Vale_Hash_SHA2_256_finish(state, dst);
 }
-
-//
-// AES
-//
-
-void crypto_vale_aes_keyExpansion(uint8_t *k, uint8_t *w, uint8_t *sb)
-{
-  Vale_AES_keyExpansion(k, w, sb);
-}
-
-void crypto_vale_aes_cipher(uint8_t *out, uint8_t *in, uint8_t *w, uint8_t *sb)
-{
-  Vale_AES_cipher(out, in, w, sb);
-}
-
